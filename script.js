02d9811b4682ba0aabde13d32129edc3a32bc245
ChetanAgrawal11 <chetanagrawal117@gmail.com>
2024-02-25 11:43:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,140 @@
+var data = [
+  {
+    name: "Dr. Ibhrahim Almajed ",
+    image:
+      "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+  },
+  {
+    name: "Dr.Mhanna Aljabab ",
+    image:
+      "https://img.freepik.com/free-photo/african-medical-doctor-man-isolated-white-background_93675-128521.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708560000&semt=ais",
+  },
+  {
+    name: "Dr.Wael Alomari",
+    image:
+      "https://t3.ftcdn.net/jpg/02/60/04/08/360_F_260040863_fYxB1SnrzgJ9AOkcT0hoe7IEFtsPiHAD.jpg",
+  },
+  {
+    name: "Dr. John Paul ",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJn7h4zNSvU73nq2u3vbJ3HP_MmSjyygJdGA&usqp=CAU",
+  },
+  {
+    name: "Dr. Najwa Joharji",
+    image: "https://www.collinsdictionary.com/images/full/doctor_117169531.jpg",
+  },
+  {
+    name: "Dr. Ibhrahim Almajed ",
+    image:
+      "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+  },
+  {
+    name: "Dr.Mhanna Aljabab ",
+    image:
+      "https://img.freepik.com/free-photo/african-medical-doctor-man-isolated-white-background_93675-128521.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708560000&semt=ais",
+  },
+  {
+    name: "Dr.Wael Alomari",
+    image:
+      "https://t3.ftcdn.net/jpg/02/60/04/08/360_F_260040863_fYxB1SnrzgJ9AOkcT0hoe7IEFtsPiHAD.jpg",
+  },
+  {
+    name: "Dr. John Paul ",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJn7h4zNSvU73nq2u3vbJ3HP_MmSjyygJdGA&usqp=CAU",
+  },
+  {
+    name: "Dr. Najwa Joharji",
+    image: "https://www.collinsdictionary.com/images/full/doctor_117169531.jpg",
+  },
+  {
+    name: "Dr. Ibhrahim Almajed ",
+    image:
+      "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+  },
+  {
+    name: "Dr.Mhanna Aljabab ",
+    image:
+      "https://img.freepik.com/free-photo/african-medical-doctor-man-isolated-white-background_93675-128521.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708560000&semt=ais",
+  },
+  {
+    name: "Dr.Wael Alomari",
+    image:
+      "https://t3.ftcdn.net/jpg/02/60/04/08/360_F_260040863_fYxB1SnrzgJ9AOkcT0hoe7IEFtsPiHAD.jpg",
+  },
+  {
+    name: "Dr. John Paul ",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJn7h4zNSvU73nq2u3vbJ3HP_MmSjyygJdGA&usqp=CAU",
+  },
+  {
+    name: "Dr. Najwa Joharji",
+    image: "https://www.collinsdictionary.com/images/full/doctor_117169531.jpg",
+  },
+  {
+    name: "Dr. Ibhrahim Almajed ",
+    image:
+      "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+  },
+  {
+    name: "Dr.Mhanna Aljabab ",
+
+    image:
+      "https://img.freepik.com/free-photo/african-medical-doctor-man-isolated-white-background_93675-128521.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708560000&semt=ais",
+  },
+  {
+    name: "Dr.Wael Alomari",
+    image:
+      "https://t3.ftcdn.net/jpg/02/60/04/08/360_F_260040863_fYxB1SnrzgJ9AOkcT0hoe7IEFtsPiHAD.jpg",
+  },
+  {
+    name: "Dr. John Paul ",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJn7h4zNSvU73nq2u3vbJ3HP_MmSjyygJdGA&usqp=CAU",
+  },
+  {
+    name: "Dr. Najwa Joharji",
+    image: "https://www.collinsdictionary.com/images/full/doctor_117169531.jpg",
+  },
+  {
+    name: "Dr. Ibhrahim Almajed ",
+    image:
+      "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=",
+  },
+  {
+    name: "Dr.Mhanna Aljabab ",
+    image:
+      "https://img.freepik.com/free-photo/african-medical-doctor-man-isolated-white-background_93675-128521.jpg?size=626&ext=jpg&ga=GA1.1.1700460183.1708560000&semt=ais",
+  },
+  {
+    name: "Dr.Wael Alomari",
+    image:
+      "https://t3.ftcdn.net/jpg/02/60/04/08/360_F_260040863_fYxB1SnrzgJ9AOkcT0hoe7IEFtsPiHAD.jpg",
+  },
+  {
+    name: "Dr. John Paul ",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJn7h4zNSvU73nq2u3vbJ3HP_MmSjyygJdGA&usqp=CAU",
+  },
+];
+function load() {
+  var container = document.getElementById("hello1");
+  data.forEach(function (item) {
+    var div = document.createElement("div");
+    div.className = "flex flex-col items-center";
+
+    var img = document.createElement("img");
+    img.src = item.image;
+    img.className = "h-64 w-64 rounded-full";
+    div.appendChild(img);
+
+    var p = document.createElement("p");
+    p.textContent = item.name;
+    p.className = "mt-4 text-lg font-semibold";
+    div.appendChild(p);
+
+    container.appendChild(div);
+  });
+}
+
+window.onload = load;
